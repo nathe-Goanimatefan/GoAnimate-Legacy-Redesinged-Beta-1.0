ec677dbbbc042ee62c9634d20878d143dfda753f
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-05-14 20:53:50 -0400

    Update util.js

--- misc/util.js
+++ misc/util.js
@@ -1,5 +1,5 @@
 module.exports = {
-	xmlFail(message = "You're grounded.") {
+	xmlFail(message = "Somthing must have failed when trying to do somthing. please try again later or use somthing different.") {
 		return `<error><code>ERR_ASSET_404</code><message>${message}</message><text></text></error>`;
 	},
 };
